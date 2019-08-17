5175e9e5685ba955f83490d68d2d3a71ee5c443d
Matt <surfsnow1@gmail.com>
2019-08-17 16:35:14 -0700

    added coordinate display to board render

--- board.rb
+++ board.rb
@@ -2,6 +2,8 @@ require_relative "tile.rb"
 
 class Board
   def initialize(size, bomb_count)
+    raise "Board size too large" if size > 26
+    @alpha = ("A".."Z").to_a
     @size = size
     @grid = Array.new(size) { Array.new(size) { Tile.new }}
     @bomb_count = bomb_count
@@ -9,17 +11,20 @@ class Board
 
   def render
     system("clear")
-    puts " Minesweeper "
-    puts "+" + ("-" * (2 * @size - 1)) + "+"
-    @grid.each do |row|
-      print "|"
+    puts "    Minesweeper "
+    puts "  +" + ("-" * (2 * @size - 1)) + "+"
+    @grid.each_with_index do |row, row_idx|
+      print row_idx < 10 ? " #{row_idx} |" : "#{row_idx} |" 
       row.each do |tile|
         print tile.to_s
         print "|"
       end
       puts 
     end
-    puts "+" + ("-" * (2 * @size - 1)) + "+"
+    puts "   +" + ("-" * (2 * @size - 1)) + "+"
+    print "    "
+    @size.times { |num| print @alpha[num] + " " }
+    puts
   end
 
 
@@ -27,5 +32,5 @@ class Board
 
 end
 
-game = Board.new(8, 3)
+game = Board.new(27, 3)
 game.render
