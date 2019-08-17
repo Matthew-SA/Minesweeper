245c2492e83572d2608d90a05c82ff12c1e05c9d
Matt <surfsnow1@gmail.com>
2019-08-17 14:53:29 -0700

    add renderer for board

--- board.rb
+++ board.rb
@@ -2,14 +2,24 @@ require_relative "tile.rb"
 
 class Board
   def initialize(size, bomb_count)
-    @grid = Array.new(size) { Array.new(size) { 0 }}
+    @size = size
+    @grid = Array.new(size) { Array.new(size) { Tile.new }}
     @bomb_count = bomb_count
   end
 
   def render
+    system("clear")
+    puts " Minesweeper "
+    puts "+" + ("-" * (2 * @size - 1)) + "+"
     @grid.each do |row|
-      p row
+      print "|"
+      row.each do |tile|
+        print tile.to_s
+        print "|"
+      end
+      puts 
     end
+    puts "+" + ("-" * (2 * @size - 1)) + "+"
   end
 
 
@@ -17,5 +27,5 @@ class Board
 
 end
 
-game = Board.new(9, 5)
+game = Board.new(8, 3)
 game.render
--- tile.rb
+++ tile.rb
@@ -5,4 +5,9 @@ class Tile
     @bomb = false
     @flag = false
   end
+
+  def to_s
+    @value
+  end
+
 end
