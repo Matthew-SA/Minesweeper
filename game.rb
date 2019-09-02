299743d83cf64d9e740b9c32bc5b00e427e6986c
Matt <surfsnow1@gmail.com>
2019-09-02 15:18:42 -0700

    fixed master game.rb

--- game.rb
+++ game.rb
@@ -1,131 +1,61 @@
 require_relative "board"
 require "yaml"
-require "remedy"
 
 class Game
-  include Remedy
   def initialize(size, bomb_count)
     raise "Bomb count must be larger than 0" if bomb_count <= 0
     raise "Bomb count too high for board" if bomb_count > (size ** 2)
     @board = Board.new(size, bomb_count)
     @board.seed_bombs(bomb_count)
-    @image = @board.render
-    self.listen
+    @alpha = ("A"..("A".ord + size).chr).to_a
+    self.run
   end
 
-  # def run
-  #   while !end?
-  #     self.listen
-
-  #   end
-  # end
-
-  def listen
-    Console.set_console_resized_hook! do
-      draw
-    end
-    # create an interaction object to handle user input
-    input = Interaction.new
-    # call draw here because interaction blocks until it gets input
-    draw
+  def run
     while !end?
-      input.loop do |key|
-        @last_key = key
-        case
-          when key == "q"
-            input.quit!
-          when key == "\e[A"
-            @board.current_coord[0] -= 1
-          when key == "\e[B"
-            @board.current_coord[0] += 1
-          when key == "\e[C"
-            @board.current_coord[1] += 1
-          when key == "\e[D"
-            @board.current_coord[1] -= 1
-          when key == "\r"
-            @board.flip_tile(@board.current_coord)
-          else
-            "invalid input!"
-          end
-          @image = @board.render
-        draw
+      @board.render
+      input = get_input
+      next if !valid_input?(input)
+      if input == "mode"
+        @board.toggle_flag_mode
+      elsif input == "save"
+        self.save_game
+      elsif input == "load"
+        self.load_game
+      else
+        pos = convert_coordinate(input)
+        @board.flip_tile(pos)
       end
     end
   end
 
-  def header
-    Header.new << "Minesweeper"
+  def toggle_flag_mode
+    @flag_mode = !@flag_mode
+    puts "worked"
+    sleep(2)
   end
 
-  def content
-    c = Partial.new
-    c << 
-    <<-CONTENT
-    #{@image}
-    #{@board.current_coord}
-    CONTENT
-    c
+  def get_input
+      puts "*** 'save' to save game.  'load' to load game.  'mode' to toggle flag_mode. ***"
+      print "Enter coordinate (e.g. a9): "
+      input = gets.chomp
   end
 
-  def footer
-    Footer.new << "You pressed: #{@last_key}, #{@board.current_coord}"
+  def valid_input?(input)
+    return true if input == "mode"
+    return true if input == "save"
+    return true if input == "load"
+    return true if @alpha.include?(input[0].upcase) && input[1..-1].to_i >= 0 && input[1..-1].to_i < (@board.size)
+    puts "Invalid input!"
+    sleep(2)
+    false
   end
 
-  def draw
-    Viewport.new.draw content, Size([0,0]), header, footer
+  def convert_coordinate(coordinate)
+    col = @alpha.find_index(coordinate[0].upcase)
+    row = coordinate[1..-1].to_i
+    [row, col]
   end
-
-
-
-
-
-
-
-
-  # def run
-  #   while !end?
-  #     @board.render
-  #     input = get_input
-  #     next if !valid_input?(input)
-  #     if input == "mode"
-  #       @board.toggle_flag_mode
-  #     elsif input == "save"
-  #       self.save_game
-  #     elsif input == "load"
-  #       self.load_game
-  #     else
-  #       pos = convert_coordinate(input)
-  #       @board.flip_tile(pos)
-  #     end
-  #   end
-  # end
-
-  # def toggle_flag_mode
-  #   @flag_mode = !@flag_mode
-  #   sleep(2)
-  # end
-
-  # def get_input
-  #     puts "*** 'save' to save game.  'load' to load game.  'mode' to toggle flag_mode. ***"
-  #     print "Enter coordinate (e.g. a9): "
-  #     input = gets.chomp
-  # end
-
-  # def valid_input?(input)
-  #   return true if input == "mode"
-  #   return true if input == "save"
-  #   return true if input == "load"
-  #   return true if @alpha.include?(input[0].upcase) && input[1..-1].to_i >= 0 && input[1..-1].to_i < (@board.size)
-  #   puts "Invalid input!"
-  #   sleep(2)
-  #   false
-  # end
-
-  # def convert_coordinate(coordinate)
-  #   col = @alpha.find_index(coordinate[0].upcase)
-  #   row = coordinate[1..-1].to_i
-  #   [row, col]
-  # end
 
   def save_game
     File.open("saves.yml", "w") { |file| file.write(@board.to_yaml)}
@@ -163,4 +93,4 @@ class Game
   end
 end
 
-game = Game.new(9, 10)
+game = Game.new(10, 20)
